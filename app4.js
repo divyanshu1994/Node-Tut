ddac7c3d05207e1701d581b5e4f2c975f81377f1
Divyanshu <sundriyald1994@gmail.com>
2016-07-08 14:11:43 +0530

    node core emitter

---
+++ app4.js
@@ -0,0 +1,14 @@
+// using node internal event emmiter
+
+var Emmiter=require('events');
+var eventsType=require('./events_config').events; // to avoid typo errors
+
+var emtr=new Emmiter();
+
+emtr.on(eventsType.LOG,function()
+{
+    console.log("Some has typed");
+});
+
+console.log("Hi");
+emtr.emit(eventsType.LOG);
