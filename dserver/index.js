afbac4b4eb2744d0881a9b269c3aeebc93ba4b34
Divyanshu <sundriyald1994@gmail.com>
2016-07-27 13:56:36 +0530

    changes

--- dserver/index.js
+++ dserver/index.js
@@ -49,33 +49,3 @@ DServer.prototype.createServer=function(port,host)
 }
 
 module.exports=DServer;
-
-// http.createServer(function(req,res)
-// {
-//     if(req.url==='/')
-//     {
-//     res.writeHead(200,{'Content-Type':'text/html'});
-//     //var html=fs.readFileSync(__dirname+"/index.html"); 
-//     //this is synchronus
-//     //res.end(html);
-
-//     //better and less memory using stream
-//     //create a stream , it will pass junk (packet ) into pipe as soon as it recive one
-//     //res is a writable stream
-//     fs.createReadStream(__dirname+'/index.html').pipe(res);
-//     }
-
-//     else if(req.url=='/jsons')
-//     {
-//         res.writeHead(200,{'Content-Type':'application/json'});
-//         var obj={
-//             name:"divyanshu",
-//             college:"IITD"
-//         }
-//         res.end(JSON.stringify(obj));
-//     }
-//     else{
-//         res.writeHead(404,{'Content-Type':'text/plain'});
-//         res.end("Page not exist");
-//     }
-// }).listen(1337,'127.0.0.1');
